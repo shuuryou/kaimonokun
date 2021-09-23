393bc65ba975f0e3dd970d3b8d8fa212c181276b
shuuryou <36278767+shuuryou@users.noreply.github.com>
2021-09-23 22:37:59 +0200

    Create config.php
    Clean config.php with AUTHLINE blanked.

---
+++ config.php
@@ -0,0 +1,39 @@
+<?php
+define('LISTS_DIR', 'lists');
+define('LIST_INDEX_FILENAME', 'listindex');
+
+define('AUTHLINE', 'changeme');
+
+// ユーザーに表示される様々なメッセージ
+define('T_PAGE_TITLE', '買い物くん');
+define('T_REQUIRES_JAVASCRIPT', '買い物くんを利用するためにはJavaScriptを有効にする必要があります。');
+define('T_ALL_LISTS', 'すべてのリスト');
+define('T_WRITE_ITEM_TEXT', 'アイテムを入力、⏎を押してね');
+define('T_WRITE_LIST_TEXT', '名前を入力、⏎を押してね');
+define('T_AJAX_ERROR_TITLE', 'ネットワークエラー');
+define('T_AJAX_ERROR_GENERIC_TEXT', 'ネットワークエラーが発生した。インターネットに接続している？サーバーの応答: ');
+define('T_EVENTSOURCE_ERROR_TITLE', 'ネットワークエラー');
+define('T_EVENTSOURCE_ERROR_GENERIC_TEXT', 'ライブアップデートができなくなった。インターネットの接続が不安定？');
+define('T_LIST_WAS_DELETED_TITLE', 'リストの削除');
+define('T_LIST_WAS_DELETED', '表示のリストがもう削除されたよ。');
+define('T_BUTTON_ADDTOGGLE', '追加');
+define('T_BUTTON_SORT', '並べ替え');
+define('T_BUTTON_BACK', '戻る');
+define('T_BUTTON_SAVE', '保存');
+define('T_BUTTON_EDIT', '編集');
+define('T_BUTTON_ERASE', '消去');
+define('T_BUTTON_CLOSE', 'OK');
+
+// 翻訳しなくてもいい内部エラーメッセージ
+define('T_ERR_MISSING_ACTION', 'Missing action.');
+define('T_ERR_MISSING_LIST_NAME', 'Missing list name.');
+define('T_ERR_MISSING_NEW_LIST_NAME', 'Missing new list name.');
+define('T_ERR_MISSING_LIST_ID', 'Missing list ID.');
+define('T_ERR_LIST_NO_LONGER_EXISTING', 'This list no longer exists.');
+define('T_ERR_MISSING_ITEM', 'Missing item.');
+define('T_ERR_MISSING_ITEM_ID', 'Missing item ID.');
+define('T_ERR_MISSING_NEW_ITEM', 'Missing new item.');
+define('T_ERR_MISSING_CHECKED', 'Missing checked state.');
+define('T_ERR_MISSING_NEW_ORDER', 'Missing new order.');
+define('T_ERR_NEW_ORDER_INVALID', 'The new order is invalid.');
+define('T_ERR_MISSING_WATCH', 'Missing watch argument.');
